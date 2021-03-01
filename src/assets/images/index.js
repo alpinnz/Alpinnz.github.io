4dfab33b7cfce51b1a2f05f6d9c429663c6f1730
Alpinnz <alpinnz@gmail.com>
2021-03-01 09:13:46 +0700

    add portofolio TA

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -5,8 +5,8 @@ const Images = {
   portfolio_2: require("./portfolio-2.jpg").default,
   portfolio_3: require("./portfolio-3.jpg").default,
   portfolio_4: require("./portfolio-4.jpg").default,
-  // portfolio_5: require("./portfolio-5.jpg").default,
-  // portfolio_6: require("./portfolio-6.jpg").default,
+  portfolio_5: require("./portfolio-5.jpg").default,
+  portfolio_6: require("./portfolio-6.jpg").default,
 };
 
 export default Images;
--- src/components/Portofolio.js
+++ src/components/Portofolio.js
@@ -14,7 +14,11 @@ const Portofolio = () => {
         </div>
         <div className="row row-value">
           <div className="col-sm-4">
-            <a href="https://github.com/alpinnz/Seleksi-Bootcamp-Arkademy-Batch-14-1">
+            <a
+              href="https://github.com/alpinnz/Seleksi-Bootcamp-Arkademy-Batch-14-1"
+              style={{ display: "table-cell" }}
+              target="_blank"
+            >
               <img
                 src={Images.portfolio_1}
                 style={{ height: "15rem" }}
@@ -24,52 +28,72 @@ const Portofolio = () => {
             </a>
           </div>
           <div className="col-sm-4">
-            <a href="https://github.com/alpinnz/kamis-server-side">
+            <a
+              href="https://github.com/alpinnz/kamis-server-side"
+              style={{ display: "table-cell" }}
+              target="_blank"
+            >
               <img
                 src={Images.portfolio_2}
                 style={{ height: "15rem" }}
                 className="img-fluid img-thumbnail"
-                alt="portofolio 1"
+                alt="portofolio 2"
               />
             </a>
           </div>
           <div className="col-sm-4">
-            <a href="https://github.com/alpinnz/web-online-shop">
+            <a
+              href="https://github.com/alpinnz/web-online-shop"
+              style={{ display: "table-cell" }}
+              target="_blank"
+            >
               <img
                 src={Images.portfolio_3}
                 style={{ height: "15rem" }}
                 className="img-fluid img-thumbnail"
-                alt="portofolio 1"
+                alt="portofolio 3"
               />
             </a>
           </div>
           <div className="col-sm-4">
-            <a href="https://github.com/alpinnz">
+            <a
+              href="https://github.com/alpinnz"
+              style={{ display: "table-cell" }}
+              target="_blank"
+            >
               <img
                 src={Images.portfolio_4}
                 style={{ height: "15rem" }}
                 className="img-fluid img-thumbnail"
-                alt="portofolio 1"
+                alt="portofolio 4"
               />
             </a>
           </div>
           <div className="col-sm-4">
-            <a href="https://github.com/alpinnz">
+            <a
+              href="http://alpinnz.xyz/customer/init/2"
+              style={{ display: "table-cell" }}
+              target="_blank"
+            >
               <img
-                src={Images.portfolio_1}
+                src={Images.portfolio_5}
                 style={{ height: "15rem" }}
                 className="img-fluid img-thumbnail"
-                alt="portofolio 1"
+                alt="portofolio 5"
               />
             </a>
           </div>
           <div className="col-sm-4">
-            <a href>
+            <a
+              href="http://alpinnz.xyz/customer/init/2"
+              style={{ display: "table-cell" }}
+              target="_blank"
+            >
               <img
-                src={Images.portfolio_1}
+                src={Images.portfolio_6}
                 style={{ height: "15rem" }}
                 className="img-fluid img-thumbnail"
-                alt="portofolio 1"
+                alt="portofolio 6"
               />
             </a>
           </div>
